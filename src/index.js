5a53a9b607fc223b5b57dac6fd56dc1b1c179a2d
nickolyamba <nickolyamba@yahoo.com>
2017-10-08 14:08:36 -0700

    feature: Async fetch posts and categories before app rendering

--- src/index.js
+++ src/index.js
@@ -4,13 +4,20 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware} from 'redux'
 import reducer from './reducers';
+import {fetchPosts} from './actions/post_actions';
+import {fetchCategories} from './actions/category_actions';
+import thunk from 'redux-thunk';
 
 const store = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(thunk)
 );
+
+store.dispatch(fetchPosts('posts'));
+store.dispatch(fetchCategories('categories'));
 
 ReactDOM.render(
     <Provider store={store}>
