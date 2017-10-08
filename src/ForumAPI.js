2c14f88362b0294760e33b7e2e375409f66673b5
nickolyamba <nickolyamba@yahoo.com>
2017-10-08 14:09:44 -0700

    feature: Add getComments related to a particular post

--- src/ForumAPI.js
+++ src/ForumAPI.js
@@ -7,20 +7,19 @@ let token = localStorage.token;
 if (!token)
     token = localStorage.token = Math.random().toString(36).substr(-8);
 
-const headers = {
-    'Authorization': token
-};
+const headers = {'Authorization': token};
+const postEntity = 'posts';
+const commentEntity = 'comments';
 
 export const get = (entityName, id) =>
     fetch(`${api}/${entityName}/${id}`, { headers })
         .then(res => res.json())
-        //.then(data => data[entityName])
+        .then(data => data[entityName])
         .catch(err => console.error('Error in fetch: \n', err));
 
 export const getAll = (entityName) =>
     fetch(`${api}/${entityName}`, { headers })
         .then(res => res.json());
-        //.then(data => data.books);
 
 export const update = (entityName, id, object) =>
     fetch(`${api}/${entityName}/${id}`, {
@@ -42,13 +41,10 @@ export const create = (entityName, object) =>
         body: JSON.stringify({ object })
     }).then(res => res.json());
 
-export const search = (query, maxResults) =>
-    fetch(`${api}/search`, {
-        method: 'POST',
-        headers: {
-            ...headers,
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ query, maxResults })
-    }).then(res => res.json())
-        .then(data => data.books);
+export const getComments = (postId) => {
+    fetch(`${api}/${postEntity}/${postId}/${commentEntity}`, { headers })
+        .then(res => res.json())
+        .catch(err => console.error('Error in fetch: ', err));
+};
+
+export default {getAll, get, getComments, update, create}
