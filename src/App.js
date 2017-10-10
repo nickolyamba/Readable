3ccdcc6a8ff8baa31f1df1f9bfa0031907c959d5
nickolyamba <nickolyamba@yahoo.com>
2017-10-09 20:35:42 -0700

    feature: Add header styling and new container

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
-import Category from './components/Category';
 import Header from './components/Header';
 import PostList from './components/PostList';
+import {Switch, Route } from 'react-router-dom';
 
 /*https://material-ui-1dab0.firebaseapp.com/demos/buttons/*/
 class App extends Component {
-  render() {
+  render(){
     return (
       <div className="App">
           <Header headerText='Readable'/>
-          <div className="container">
-              <Category/>
-              <PostList/>
+          <div className="ctrl-container">
+            <h3>List of Posts</h3>
+          </div>
+          <div className="vertical-container">
+              <Switch>
+                  <Route exact path='/' component={PostList}/>
+                  <Route path='/:category' component={PostList}/>
+              </Switch>
           </div>
       </div>
     );
