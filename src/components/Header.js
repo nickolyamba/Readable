1fe7a405fd317e3578c7d20b8ddbe2d913d00b32
nickolyamba <nickolyamba@yahoo.com>
2017-10-09 20:41:23 -0700

    feature: Add category navigation to navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux';
+import { changeCategory } from '../actions/category_actions';
 
-const Header = (props) => (
+class Header extends React.Component {
+    state = {selected: this.props.selectedCategory};
 
-    <nav className="header">
-        <h1>{props.headerText}</h1>
-    </nav>
+    render() {
+        const {categories, updateCategory} = this.props;
+        return (
+            <nav className="header">
+                {categories && categories.map((category, i) => (
+                    <Link to={`/${category.path}`} key={i}
+                          onClick={() => {updateCategory(category.name); this.setState({selected: category.path})}}>
+                        <h1 className={this.state.selected === category.path ? 'activeLink' : null}>{category.name}</h1>
+                    </Link>
+                ))}
+            </nav>
+        )
 
-);
+    }
+}
 
 Header.propTypes = {
     headerText: PropTypes.string.isRequired
 };
 
-export default Header;
+const mapStateToProps = ({categories}) => {
+    return {
+        categories: categories.entities ? Object.values(categories.entities) : null,
+        selectedCategory: categories.selected ? categories.selected : null
+    }
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        updateCategory: (selectedCategory) => dispatch(changeCategory(selectedCategory))
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
