9ff9b71c2166bfb7d327f0d15df377b21aaf47ab
nickolyamba <nickolyamba@yahoo.com>
2017-10-16 01:40:21 -0700

    feat: Enclose form in collapse component

--- src/components/PostDetailsView.js
+++ src/components/PostDetailsView.js
@@ -5,23 +5,47 @@ import { connect } from 'react-redux';
 import Typography from 'material-ui/Typography';
 import CommentList from './CommentList'
 import { changeCategory } from '../actions/category_actions';
+import CardForm from './CardForm'
+import Button from 'material-ui/Button';
+import Collapse from 'material-ui/transitions/Collapse';
 
 class PostDetailsView extends Component {
+    state = {expanded: false};
+
     componentDidMount(){
         const {post, updateCategory} = this.props;
         post && updateCategory(post.category);
     }
 
+    handleCollapse = () => {
+        this.setState({expanded: !this.state.expanded});
+    };
+
     render(){
         const {post} = this.props;
         return (
             <div>
+                <Typography type="title" gutterBottom align="center" color="secondary">
+                    Post Details
+                </Typography>
                 {post && !post.deleted &&
                 <div>
                     <Entry entry={post} entityName={'posts'} isDetailsView/>
-                    <Typography type="title" gutterBottom align="center" color="secondary">
-                        Comments
-                    </Typography>
+
+                    <div className="containerSpread marginTop50">
+                        <Typography type="title" gutterBottom align="center" color="secondary">
+                            Comments
+                        </Typography>
+                        <Button onClick={this.handleCollapse} aria-expanded={this.state.expanded}
+                                color='primary'>
+                            Add Comment
+                        </Button>
+                    </div>
+
+                    <Collapse in={this.state.expanded} transitionDuration="auto" unmountOnExit>
+                        <CardForm parentId={post.id} closeCollapse={() => this.handleCollapse()}/>
+                    </Collapse>
+
                     <CommentList postId={post.id}/>
                 </div>
                 }
--- src/components/PostListView.js
+++ src/components/PostListView.js
@@ -16,9 +16,6 @@ class PostListView extends Component {
         const selectedCategory = this.props.match.params.category;
         return (
             <div>
-                <div className="ctrlContainer">
-                    <h3>List of Posts</h3>
-                </div>
                 <SortControl sortCategories={{date: 'timestamp', vote: 'voteScore'}}/>
                 <PostList selectedCategory={selectedCategory}/>
             </div>
