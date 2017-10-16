6ce356ea3efdeb73089bc15385e414825f2710e3
nickolyamba <nickolyamba@yahoo.com>
2017-10-15 17:55:31 -0700

    feat: Add CommentList related to the given Post

--- src/components/PostDetailsView.js
+++ src/components/PostDetailsView.js
@@ -3,17 +3,22 @@ import Entry from './Entry';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Typography from 'material-ui/Typography';
+import CommentList from './CommentList'
 
 class PostDetailsView extends Component {
     render(){
-        const {entry} = this.props;
+        const {post} = this.props;
         return (
             <div>
-                <Entry entry={entry} entityName={'posts'} isDetailsView/>
-                <Typography type="title" gutterBottom align="center" color="secondary">
-                    Comments
-                </Typography>
-
+                {post &&
+                <div>
+                    <Entry entry={post} entityName={'posts'} isDetailsView/>
+                    <Typography type="title" gutterBottom align="center" color="secondary">
+                        Comments
+                    </Typography>
+                    <CommentList postId={post.id}/>
+                </div>
+                }
             </div>
         );
     }
@@ -24,11 +29,12 @@ PostDetailsView.propTypes = {
         params: PropTypes.shape({
             postId: PropTypes.string.isRequired
         })
-    })
+    }),
+    post: PropTypes.object
 };
 
-const mapStateToProps = ({posts, comments}, ownProps) => ({
-    entry: posts.entities ? posts.entities[ownProps.match.params.postId] : null
+const mapStateToProps = ({posts}, ownProps) => ({
+    post: posts.entities ? posts.entities[ownProps.match.params.postId] : null
 });
 
 export default connect(mapStateToProps)(PostDetailsView);
